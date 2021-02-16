9120d3455792fedf8266e8e088f2130759d81b3b
keika <jesicamorocho.m@gmail.com>
2021-02-16 20:56:44 +0100

    añadida funcion id interno, borrada funcion id externo, hecha segunda pantalla para mostrar busquedas

--- js/main.js
+++ js/main.js
@@ -32,16 +32,60 @@ const searchFilms = () => {
     fetch(`https://api.themoviedb.org/3/search/movie?api_key=e34f732b92a2e7dbe69709d0433150c3&language=es&query=${query}`)
     .then(response => response.json())
     .then(data => {
-        console.log(data.results);
+        let movies = data.results;
+        renderSearch(movies)
     })   
 }
-
-//BUSCAR PELÍCULAS X id
+//PINTAR PELICULAS BUSCADAS POR NOMBRE
+const renderSearch = (movies) => {
+    movies.forEach(
+        movie => document.querySelector('#screen2').innerHTML+=
+    `<div id="film-Container">
+          <h2 id="tittle-film">${movie.original_title}</h2>
+         <img id="photo-film" src="https://image.tmdb.org/t/p/w185${movie.poster_path}">
+         </div>`
+        ) 
+         cambiaPantalla("screen1","screen2");     
+}
+//BUSCAR PELÍCULAS POR ID
 const searchId = () => {
+    let id = inputId.value;
+    fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=e34f732b92a2e7dbe69709d0433150c3&language=es`)
+    .then(response => response.json())
+    .then(data => {
+         let movies = data;
+        renderId(movies)
+    })
+} 
+//PINTAR PELICULAS BUSCADAS POR ID
+const renderId = (movies) => {
+    document.querySelector('#screen2').innerHTML+=
+    `<div id="film-Container">
+          <h2 id="tittle-film">${movies.original_title}</h2>
+         <img id="photo-film" src="https://image.tmdb.org/t/p/w185${movies.poster_path}">
+         </div>`
+         cambiaPantalla("screen1","screen2");     
+
+}
+// FUNCION CAMBIAR DE PANTALLA
+
+let cambiaPantalla = (faseAhora,faseFutura) => {
+    let pantallaActual = document.getElementById(faseAhora);
+
+    let pantallaDestino = document.getElementById(faseFutura);
+
+    //cambiamos visibiidad pantallas
+
+    pantallaActual.style.display = "none";
+    pantallaDestino.style.display = "block";
+};
+
+//BUSCAR PELÍCULAS X id externo
+/* const searchId = () => {
     let id = inputId.value;
     fetch(`https://api.themoviedb.org/3/find/${id}?api_key=e34f732b92a2e7dbe69709d0433150c3&language=es&external_source=imdb_id`)
     .then(response => response.json())
     .then(data => {
         console.log(data.movie_results[0].original_title);
     })   
-}
+} */
